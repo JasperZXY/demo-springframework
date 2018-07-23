35eb303eed7a873c03950c0ced63d19f334d3abb
ruanwei <ruanwei@192.168.0.7>
2018-07-23 19:04:48 +0800

    init

--- demo-springframework-temp/src/main/java/org/ruanwei/demo/springframework/core/aop/Good3.java
+++
@@ -1,5 +0,0 @@
-package org.ruanwei.demo.springframework.core.aop;
-
-public interface Good3 {
-	String good(String msg);
-}
--- demo-springframework-temp/src/main/java/org/ruanwei/demo/springframework/core/aop/Happy3.java
+++
@@ -1,5 +0,0 @@
-package org.ruanwei.demo.springframework.core.aop;
-
-public interface Happy3 {
-    String happy(String msg);
-}
