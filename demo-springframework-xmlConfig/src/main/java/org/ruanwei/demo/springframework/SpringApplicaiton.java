021b36dded87c3969a88cc3d93617f920fbf69f8
ruanwei <ruanwei@192.168.0.7>
2018-07-29 20:03:29 +0800

    init

---
+++ demo-springframework-xmlConfig/src/main/java/org/ruanwei/demo/springframework/SpringApplicaiton.java
@@ -0,0 +1,301 @@
+package org.ruanwei.demo.springframework;
+
+import java.sql.Date;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.ruanwei.demo.springframework.core.aop.Good;
+import org.ruanwei.demo.springframework.core.ioc.Family;
+import org.ruanwei.demo.springframework.core.ioc.House;
+import org.ruanwei.demo.springframework.core.ioc.event.MyApplicationEvent;
+import org.ruanwei.demo.springframework.core.ioc.extension.MyFamilyFactoryBean;
+import org.ruanwei.demo.springframework.dataAccess.User;
+import org.ruanwei.demo.springframework.dataAccess.jdbc.JdbcDAO;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.AbstractRefreshableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.MapPropertySource;
+import org.springframework.core.env.MutablePropertySources;
+import org.springframework.core.env.PropertySource;
+import org.springframework.core.env.StandardEnvironment;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+
+public class SpringApplicaiton {
+	private static Log log = LogFactory.getLog(SpringApplicaiton.class);
+
+	private static AbstractApplicationContext context;
+
+	private static final User paramForCreate1 = new User("ruanwei_tmp", 35,
+			Date.valueOf("1983-07-06"));
+	private static final User paramForUpdate1 = new User("ruanwei", 18,
+			Date.valueOf("1983-07-06"));
+	private static final User paramForUpdate2 = new User("ruanwei_tmp", 88,
+			Date.valueOf("1983-07-06"));
+
+	private static final Map<String, Object> paramForCreate2 = new HashMap<String, Object>();
+	private static final Map<String, Object> paramForUpdate3 = new HashMap<String, Object>();
+	private static final Map<String, Object> paramForUpdate4 = new HashMap<String, Object>();
+
+	static {
+		paramForCreate2.put("name", "ruanwei_tmp");
+		paramForCreate2.put("age", 35);
+		paramForCreate2.put("birthday", Date.valueOf("1983-07-06"));
+
+		paramForUpdate3.put("name", "ruanwei");
+		paramForUpdate3.put("age", 18);
+		paramForUpdate3.put("birthday", Date.valueOf("1983-07-06"));
+
+		paramForUpdate4.put("name", "ruanwei_tmp");
+		paramForUpdate4.put("age", 88);
+		paramForUpdate4.put("birthday", Date.valueOf("1983-07-06"));
+	}
+
+	public static void main(String[] args) {
+		log.info("0======================================================================================");
+		getApplicationContext(ApplicationContextType.CLASSPATH_XML);
+
+		// testCoreContainer();
+
+		testDataAccess();
+	}
+
+	private static void testCoreContainer() {
+		log.info("1======================================================================================");
+		testEnvironment(context);
+
+		log.info("2======================================================================================");
+		testMessageSource(context);
+
+		log.info("3======================================================================================");
+		testResourceLoader(context);
+
+		log.info("4======================================================================================");
+		testApplicationEventPublisher(context);
+
+		log.info("5======================================================================================");
+		testIoC(context);
+
+		log.info("6======================================================================================");
+		testAOP(context);
+
+		log.info("7======================================================================================");
+		testApplicationEvent(context);
+	}
+
+	private static void testDataAccess() {
+		testCRUD();
+		testTransaction();
+	}
+
+	private static void testCRUD() {
+		JdbcDAO jdbcDAO = context.getBean("jdbcDAO", JdbcDAO.class);
+
+		testCreate(jdbcDAO);
+		testBatchUpdate(jdbcDAO);
+		testQueryForSingleRow(jdbcDAO);
+		testQueryFormultiRow(jdbcDAO);
+		testDelete(jdbcDAO);
+	}
+
+	private static void testCreate(JdbcDAO jdbcDAO) {
+		jdbcDAO.createUser1(paramForCreate1);
+		jdbcDAO.createUser2(paramForCreate1);
+		jdbcDAO.createUser3(paramForCreate1);
+		jdbcDAO.createUser4(paramForCreate1);
+		jdbcDAO.createUser4(paramForCreate2);
+		jdbcDAO.createUser5(paramForCreate1);
+		jdbcDAO.createUser5(paramForCreate2);
+	}
+
+	private static void testBatchUpdate(JdbcDAO jdbcDAO) {
+		List<User> users = Arrays.asList(paramForUpdate1, paramForUpdate2);
+		jdbcDAO.batchUpdateUser1(users);
+		jdbcDAO.batchUpdateUser2(users);
+		jdbcDAO.batchUpdateUser3(users);
+		jdbcDAO.batchUpdateUser4(paramForUpdate3, paramForUpdate4);
+	}
+
+	private static void testQueryForSingleRow(JdbcDAO jdbcDAO) {
+		jdbcDAO.queryForSingleColumn();
+		jdbcDAO.queryForMultiColumn();
+		jdbcDAO.queryForObject();
+	}
+
+	private static void testQueryFormultiRow(JdbcDAO jdbcDAO) {
+		jdbcDAO.queryForSingleColumnList();
+		jdbcDAO.queryForMultiColumnList();
+		jdbcDAO.queryForObjectList();
+	}
+
+	private static void testDelete(JdbcDAO jdbcDAO) {
+		jdbcDAO.deleteUser(2);
+	}
+
+	private static void testTransaction() {
+	}
+
+	private static void getApplicationContext(ApplicationContextType type) {
+		switch (type) {
+		case ANNOTATION_CONFIG: {// GenericApplicationContext
+			context = new AnnotationConfigApplicationContext(AppConfig.class);
+			// 要在getBean之前进行设置并刷新
+			// context.setConfigLocation("spring/applicationContext.xml");
+			// context.register(AppConfig.class);
+			// context.refresh();
+			break;
+		}
+		case CLASSPATH_XML: {// AbstractRefreshableApplicationContext
+			context = new ClassPathXmlApplicationContext(
+					new String[] { "classpath:spring/applicationContext.xml" });
+			break;
+		}
+		case FILESYSTEM_XML: {// AbstractRefreshableApplicationContext
+			context = new FileSystemXmlApplicationContext(
+					new String[] { "file:spring/applicationContext.xml" });
+			break;
+		}
+		default:
+		}
+
+		context.registerShutdownHook();
+	}
+
+	// StandardEnvironment/StandardServletEnvironment(spring-web)
+	private static void testEnvironment(ApplicationContext context) {
+		log.info("env==========" + context.getEnvironment());
+
+		testProfile(context);
+
+		testPropertySource(context.getEnvironment());
+	}
+
+	private static void testProfile(ApplicationContext context) {
+		Environment env = context.getEnvironment();
+		log.info("profiles==========" + env.getActiveProfiles() + " "
+				+ env.getDefaultProfiles());
+
+		if (env instanceof ConfigurableEnvironment) {
+			ConfigurableEnvironment configEnv = (StandardEnvironment) env;
+			// -Dspring.profiles.active="production"
+			// -Dspring.profiles.default="development"
+			configEnv.setActiveProfiles("development");
+			configEnv.setDefaultProfiles("production");
+		}
+
+		House house = context.getBean("house", House.class);
+		log.info("house==========" + house);
+	}
+
+	// StandardEnvironment:MapPropertySource(systemProperties)/SystemEnvironmentPropertySource(systemEnvironment)
+	private static void testPropertySource(Environment env) {
+		// TODO:ClassPathXmlApplicationContext不能获取到属性值，但是annotation那个项目却可以
+		String a = env.getProperty("a", "a"); // MapPropertySource(-Da=1)
+		String b = env.getProperty("family.familyCount", "2");// ResourcePropertySource(@PeopertySource("family.properties"))
+		String c = env.getProperty("guest.name"); // PropertySourcesPlaceholderConfigurer支持PropertySource参与占位符替换
+		log.info("property=========a=" + a + " b=" + b + " c=" + c);
+
+		if (env instanceof ConfigurableEnvironment) {
+			ConfigurableEnvironment configEnv = (ConfigurableEnvironment) env;
+
+			Map<String, Object> map = new HashMap<String, Object>();
+			map.put("key", 8);
+			PropertySource<Map<String, Object>> mapPropertySource = new MapPropertySource(
+					"mapPropertySource", map);
+
+			// EnumerablePropertySource:CommandLinePropertySource/MapPropertySource/ServletConfigPropertySource/ServletContextPropertySource
+			// CommandLinePropertySource:SimpleCommandLinePropertySource/JOptCommandLinePropertySource
+			// MapPropertySource:PropertiesPropertySource/SystemEnvironmentPropertySource
+			// PropertiesPropertySource:ResourcePropertySource("classpath:*.properties")/MockPropertySource
+			MutablePropertySources propertySources = configEnv
+					.getPropertySources();
+			propertySources.addLast(mapPropertySource);
+			log.info("PropertySources==========" + propertySources);
+		}
+
+	}
+
+	private static void testMessageSource(MessageSource messageSource) {
+		log.info("messageSource==========" + messageSource);
+		String msg = messageSource.getMessage("my.messageSource",
+				new Object[] { "ruanwei" }, "This is my message source.",
+				Locale.US);
+		log.info("message==========" + msg);
+	}
+
+	private static void testResourceLoader(ResourceLoader resourceLoader) {
+		log.info("resourceLoader==========" + resourceLoader);
+		Resource resource = resourceLoader
+				.getResource("classpath:spring/applicationContext.xml");
+		log.info("resource==========" + resource);
+	}
+
+	private static void testApplicationEventPublisher(
+			ApplicationEventPublisher applicationEventPublisher) {
+		log.info("applicationEventPublisher=========="
+				+ applicationEventPublisher);
+		applicationEventPublisher.publishEvent(new MyApplicationEvent(
+				SpringApplicaiton.class,
+				"custom ApplicationEvent from SpringApplication"));
+		applicationEventPublisher.publishEvent(new String(
+				"PayloadApplicationEvent<String> from SpringApplication"));
+	}
+
+	private static void testIoC(ApplicationContext context) {
+		Family family = context.getBean("family", Family.class);
+		Family familyx = context.getBean("familyx", Family.class);
+		MyFamilyFactoryBean myFamilyFactoryBean = (MyFamilyFactoryBean) context
+				.getBean("&familyx");
+		log.info(family);
+		log.info(familyx);
+		log.info(myFamilyFactoryBean);
+
+	}
+
+	private static void testAOP(ApplicationContext context) {
+		Family family = context.getBean("family", Family.class);
+		family.sayHello("whatever");
+
+		// TODO:Java配置下还不能正常工作
+		Good good = (Good) context.getBean("good");
+		// Happy mixin = (Happy) context.getBean("good");
+		// log.info(good.good("whatever") + mixin.happy("whatever"));
+	}
+
+	private static void testApplicationEvent(ApplicationContext context) {
+		log.info("context==========" + context);
+		if (context instanceof AbstractApplicationContext) {
+			AbstractApplicationContext absContext = (AbstractApplicationContext) context;
+			log.info("7.1======================================================================================");
+			absContext.start();
+
+			log.info("7.2======================================================================================");
+			absContext.stop();
+
+			log.info("7.3======================================================================================");
+			// 即ClassPathXmlApplicationContext和FileSystemXmlApplicationContext
+			if (context instanceof AbstractRefreshableApplicationContext) {
+				absContext.refresh();
+			}
+
+			log.info("7.4======================================================================================");
+			absContext.close();
+		}
+	}
+
+	public enum ApplicationContextType {
+		CLASSPATH_XML, FILESYSTEM_XML, ANNOTATION_CONFIG
+	}
+}
---
+++ demo-springframework-xmlConfig/src/main/java/org/ruanwei/demo/springframework/core/ioc/House.java
@@ -0,0 +1,138 @@
+package org.ruanwei.demo.springframework.core.ioc;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Required;
+
+// @Validated
+public class House {
+	private static Log log = LogFactory.getLog(House.class);
+
+	private String houseName;
+	private String hostName;
+
+	private Integer[] someArray;
+	private List<Integer> someList;
+	private Set<Integer> someSet;
+	private Properties someProperties;
+	private Map<String, Integer> someMap;
+
+	private List<Integer> someList2;
+	private Set<Integer> someSet2;
+	// 注意这里还注入family.properties
+	private Properties someProperties2;
+	private Map<String, Integer> someMap2;
+
+	private double someField1;
+	private String someField2;
+	private String someField3;
+
+	// Setter-based dependency injection
+	@Required
+	public void setHouseName(String houseName) {
+		log.info("setHouseName(String houseName)" + houseName);
+		this.houseName = houseName;
+	}
+
+	@Required
+	public void setHostName(String hostName) {
+		log.info("setHostName(String hostName)" + hostName);
+		this.hostName = hostName;
+	}
+
+	@Required
+	public void setSomeArray(Integer[] someArray) {
+		log.info("setSomeArray(Integer[] someArray)" + someArray);
+		this.someArray = someArray;
+	}
+	
+	@Required
+	public void setSomeList(List<Integer> someList) {
+		log.info("setSomeList(List<Integer> someList)" + someList);
+		this.someList = someList;
+	}
+
+	@Required
+	public void setSomeSet(Set<Integer> someSet) {
+		log.info("setSomeSet(Set<Integer> someSet)" + someSet);
+		this.someSet = someSet;
+	}
+
+	@Required
+	public void setSomeProperties(Properties someProperties) {
+		log.info("setSomeProperties(Properties someProperties)" + someProperties);
+		this.someProperties = someProperties;
+	}
+
+	@Required
+	public void setSomeMap(Map<String, Integer> someMap) {
+		log.info("setSomeMap(Map<String, Integer> someMap)" + someMap);
+		this.someMap = someMap;
+	}
+
+	@Required
+	public void setSomeList2(List<Integer> someList) {
+		log.info("setSomeList2(List<Integer> someList)" + someList);
+		this.someList2 = someList;
+	}
+
+	@Required
+	public void setSomeSet2(Set<Integer> someSet) {
+		log.info("setSomeSet2(Set<Integer> someSet)" + someSet);
+		this.someSet2 = someSet;
+	}
+
+	@Required
+	public void setSomeProperties2(Properties someProperties) {
+		log.info("setSomeProperties2(Properties someProperties)" + someProperties);
+		this.someProperties2 = someProperties;
+	}
+
+	@Required
+	public void setSomeMap2(Map<String, Integer> someMap) {
+		log.info("setSomeMap2(Map<String, Integer> someMap)" + someMap);
+		this.someMap2 = someMap;
+	}
+
+	@Required
+	public void setSomeField1(double someField) {
+		log.info("setSomeField1(double someField)" + someField);
+		this.someField1 = someField;
+	}
+
+	@Required
+	public void setSomeField2(String someField) {
+		log.info("setSomeField2(String someField)" + someField);
+		this.someField2 = someField;
+	}
+
+	@Required
+	public void setSomeField3(String someField) {
+		log.info("setSomeField3(String someField)" + someField3);
+		this.someField3 = someField;
+	}
+
+	public void init() {
+		log.info("====================init()");
+	}
+
+	public void destroy() {
+		log.info("====================destroy()");
+	}
+
+	@Override
+	public String toString() {
+		return "House [houseName=" + houseName + ", hostName=" + hostName + ", someArray=" + Arrays.toString(someArray)
+				+ ", someList=" + someList + ", someSet=" + someSet + ", someProperties=" + someProperties
+				+ ", someMap=" + someMap + ", someList2=" + someList2 + ", someSet2=" + someSet2 + ", someProperties2="
+				+ someProperties2 + ", someMap2=" + someMap2 + ", someField1=" + someField1 + ", someField2="
+				+ someField2 + ", someField3=" + someField3 + "]";
+	}
+
+}
